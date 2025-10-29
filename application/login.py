b41a09bccf1169693c409431330d3acf22665749
Samkian0007 <samkian0007@gmail.com>
2025-10-29 10:35:26 +0100

    login for wrong sceanrios has been fixed

--- application/login.py
+++ application/login.py
@@ -1,5 +1,5 @@
 import os
-from flask import Blueprint, request, redirect, url_for, flash, session, render_template
+from flask import Blueprint, request, redirect, url_for, flash, session
 from dotenv import load_dotenv
 load_dotenv()  # keep as-is
 
@@ -9,10 +9,6 @@ ENV_PASS = os.getenv("APP_PASSWORD", "")
 bp = Blueprint("auth", __name__)
 
 
-@bp.get("/login")
-def login_form():
-    return render_template("login")
-
 @bp.post("/login")
 def login():
     u = request.form.get("username", "")
@@ -21,10 +17,12 @@ def login():
         session["logged_in"] = True
         session["username"] = u
         flash("Logged in successfully.", "success")
-        return redirect(url_for("home"))
+        return redirect(url_for("login"))
     else:
-        flash("Invalid username or password. Please try again.", "error")
-        return redirect(url_for("retry_login"))                    
+        flash("Invalid username or password.", "error")
+        return redirect(url_for("home"))
+
+
 
 @bp.get("/logout")
 def logout():
